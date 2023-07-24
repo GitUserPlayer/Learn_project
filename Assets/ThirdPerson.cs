32c3f7dbea8d380a92f850a5fcbad70f4b1e25f9
Xester <BurgerRBLX@gmail.com>
2023-07-24 11:33:20 +0800

    Fixed movement bugs,completed player movement foundation.

--- Assets/ThirdPerson.cs
+++ Assets/ThirdPerson.cs
@@ -5,19 +5,25 @@ using UnityEngine;
 public class ThirdPerson : MonoBehaviour
 {
     public CharacterController controller;
+    public Rigidbody rb;
 
     public float speed = 6f;
 
     // Update is called once per frame
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
     void Update()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
-        Vector3 direction = new Vector3(horizontal, 0, vertical).normalized;   
+        Vector3 Position = GameObject.Find("Player").transform.position;
+        Vector3 direction = new Vector3(horizontal, 0, vertical);   
 
         if (direction.magnitude >= 0.1f)
         {
-            controller.Move(direction * speed * Time.deltaTime);
+         rb.velocity = direction*speed;
         }
 
     }
